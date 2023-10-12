cce998bace7dc4fb10f2e2dd0eb1ce8a6a88941c
Roberto Montufar <roberto.montufarar@gmail.com>
2023-10-12 01:37:37 -0600

    Second commit general bug fix
    Added background gradient
    Added background overlay
    Added invisible scrollfunctionality
    Completed SocialButton
    Fixed bug in SocialButton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				".no-scrollbar::-webkit-scrollbar": {
+					display: "none",
+				},
+				".no-scrollbar": {
+					"ms-overflow-style": "none",
+					"scrollbar-width": "none",
+				},
+			};
+			addUtilities(newUtilities);
+		},
+	],
 };
